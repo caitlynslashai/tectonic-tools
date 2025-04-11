06921bafbde710faab257ba8e82073ea23817a28
AlphaKretin <deewhydeeecks@gmail.com>
2025-04-11 19:27:54 +0930

    Centre Pokemon images in table headers

--- src/app/teambuilder/components/TableHeader.tsx
+++ src/app/teambuilder/components/TableHeader.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function TableHeader({ pokemon }: { pokemon: Pokemon }) {
     return (
         !isNull(pokemon) && (
-            <div>
+            <div className="flex flex-col items-center">
                 <Image src={pokemon.getImage()} alt={pokemon.name} height="80" width="80" className="my-2" />
                 <span>{pokemon.name}</span>
             </div>
