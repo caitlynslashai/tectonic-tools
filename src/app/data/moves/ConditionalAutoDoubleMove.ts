3faab14409e77f322c1e322151028f42d169d225
AlphaKretin <deewhydeeecks@gmail.com>
2025-04-27 06:15:55 +0930

    Implement Hex
    Love that we're getting into the part of this where earlier foundational work is paying off to make all these easier

--- src/app/data/moves/ConditionalAutoDoubleMove.ts
+++ src/app/data/moves/ConditionalAutoDoubleMove.ts
@@ -12,6 +12,7 @@ function punishStatus(status: StatusEffect) {
 
 const conditionalAutoDoubleMoveCodes: Record<string, ConditionFunction> = {
     DoubleDamageNoItem: (user: PartyPokemon) => user.items.filter((i) => !isNull(i)).length === 0,
+    DoubleDamageTargetStatused: (_: PartyPokemon, target: PartyPokemon) => target.statusEffect !== "None", // Does Hex interact with Volatile SEs?
     DoubleDamageAgainstPoisoned: punishStatus("Poison"),
     // Smelling Salts and Wake-Up Slap aren't identical to Venoshock,
     // but the status-curing part isn't relevant to a single turn of damage calculation
