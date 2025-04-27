4636ee5eb6f47db0090c7243e036345c6f098358
AlphaKretin <deewhydeeecks@gmail.com>
2025-04-27 14:33:02 +0930

    Implement Dream Absorb

--- src/app/data/moves/ConditionalAutoDoubleMove.ts
+++ src/app/data/moves/ConditionalAutoDoubleMove.ts
@@ -14,7 +14,8 @@ const conditionalAutoDoubleMoveCodes: Record<string, ConditionFunction> = {
     DoubleDamageNoItem: (user: PartyPokemon) => user.items.filter((i) => !isNull(i)).length === 0,
     DoubleDamageTargetStatused: (_: PartyPokemon, target: PartyPokemon) => target.statusEffect !== "None", // Does Hex interact with Volatile SEs?
     DoubleDamageAgainstPoisoned: punishStatus("Poison"),
-    // Smelling Salts and Wake-Up Slap aren't identical to Venoshock,
+    HealUserByHalfOfDamageDoneDoubleDamageIfTargetAsleep: punishStatus("Sleep"),
+    // Smelling Salts and Wake-Up Slap aren't identical to Venoshock etc,
     // but the status-curing part isn't relevant to a single turn of damage calculation
     SmellingSalts: punishStatus("Numb"),
     WakeUpSlap: punishStatus("Sleep"),
