4b476d1f292d2bca5d9fefbf3141c81275684b73
AlphaKretin <deewhydeeecks@gmail.com>
2025-06-18 14:25:22 +0930

    Enable Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
