992f04b44a3207de2a0578dde8f61311ca21ef47
AlphaKretin <deewhydeeecks@gmail.com>
2025-08-11 17:30:56 +0930

    Implement PUREFORCE and PUREENERGY
    Close #260

--- src/app/data/abilities/StatModifyAbility.ts
+++ src/app/data/abilities/StatModifyAbility.ts
@@ -5,6 +5,8 @@ import { Stat, Stats } from "../tectonic/Pokemon";
 // RADIATE works differently on live and dev - currently implementing live version
 const statModifyAbilities: Record<string, [Stat, number]> = {
     RADIATE: ["spatk", 1.3],
+    PUREFORCE: ["attack", 1.5],
+    PUREENERGY: ["spatk", 1.5],
 };
 
 export class StatModifyAbility extends Ability {
