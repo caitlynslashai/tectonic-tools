341fc0e92522f64f480bb324513618bcb8e8a73f
eseria <caitlynslashai@gmail.com>
2025-10-30 03:35:11 -0400

    implement type boost abilities

--- src/app/data/abilities/BaseDamageBoostAbility.ts
+++ src/app/data/abilities/BaseDamageBoostAbility.ts
@@ -6,10 +6,29 @@ type damageBoostConditionFunction = (move: MoveData) => boolean;
 
 const baseDamageBoostConditions: Record<string, damageBoostConditionFunction> = {
     RECKLESS: (move: MoveData) => move.move.isRecoil(),
+    SCALDINGSMOKE: (move: MoveData) => move.move.type.id === "POISON",
+    VERDANT: (move: MoveData) => move.move.type.id === "GRASS",
+    TUNNELMAKER: (move: MoveData) => move.move.type.id === "GROUND",
+    STRATAGEM: (move: MoveData) => move.move.type.id === "ROCK",
+    TOXICATTITUDE: (move: MoveData) => move.move.type.id === "POISON",
+    STEELWORKER: (move: MoveData) => move.move.type.id === "STEEL",
+    STEELYSPIRIT: (move: MoveData) => move.move.type.id === "STEEL",
+    SURFSUP: (move: MoveData) => move.move.type.id === "WATER",
+    ERUDITE: (move: MoveData) => move.move.type.id === "PSYCHIC",
+
 };
 
 const baseDamageBoostValues: Record<string, number> = {
     RECKLESS: 1.3,
+    SCALDINGSMOKE: 1.5,
+    VERDANT: 1.5,
+    TUNNELMAKER: 1.5,
+    STRATAGEM: 1.5,
+    TOXICATTITUDE: 1.5,
+    STEELWORKER: 1.5,
+    STEELYSPIRIT: 1.5,
+    SURFSUP: 1.5,
+    ERUDITE: 1.5,
 };
 
 export class BaseDamageBoostAbility extends Ability {
