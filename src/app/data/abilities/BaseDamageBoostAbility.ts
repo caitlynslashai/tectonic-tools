845846cf38cfc64644115a447fad7190faa0dc4b
caitlyn / ai <caitlynslashai@gmail.com>
2025-10-30 15:17:10 -0400

    287 brisk (#325)
    * implement wind move abilities

--- src/app/data/abilities/BaseDamageBoostAbility.ts
+++ src/app/data/abilities/BaseDamageBoostAbility.ts
@@ -6,6 +6,8 @@ type damageBoostConditionFunction = (move: MoveData) => boolean;
 
 const baseDamageBoostConditions: Record<string, damageBoostConditionFunction> = {
     RECKLESS: (move: MoveData) => move.move.isRecoil(),
+    BRISK: (move: MoveData) => move.move.flags.includes("Wind"),
+    GALEFORCE: (move: MoveData) => move.move.flags.includes("Wind"),
     HEAVYDUTYHOOVES: (move: MoveData) => move.move.flags.includes("Kick") || move.move.flags.includes("Kicking"),
     IRONHEEL: (move: MoveData) => move.move.flags.includes("Kick") || move.move.flags.includes("Kicking"),
     STRONGJAW: (move: MoveData) => move.move.flags.includes("Biting") || move.move.flags.includes("Bite"),
@@ -14,6 +16,8 @@ const baseDamageBoostConditions: Record<string, damageBoostConditionFunction> = 
 
 const baseDamageBoostValues: Record<string, number> = {
     RECKLESS: 1.3,
+    BRISK: 1.3,
+    GALEFORCE: 1.5,
     HEAVYDUTYHOOVES: 1.3,
     IRONHEEL: 1.3,
     STRONGJAW: 1.5,
